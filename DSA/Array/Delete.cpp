5825f5c3b3c38a13604ffa184be595ea7970f5cf
sudais122 <khansb17798@gmail.com>
2026-03-15 23:52:19 +0500

    array start

---
+++ DSA/Array/Delete.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {12,3,21,12,33};
+
+    int elme;
+    cout << "Enter Element";
+    cin >> elme;
+
+    for (int i = 0; i < 1; i++)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
