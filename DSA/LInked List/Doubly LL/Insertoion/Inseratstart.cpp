bb9576f42464259372b588f1e0cfc4f305682a18
sudais122 <khansb17798@gmail.com>
2026-03-24 22:27:51 +0500

    DDl insetion

--- DSA/LInked List/Doubly LL/Insertoion/Inseratstart.cpp
+++ DSA/LInked List/Doubly LL/Insertoion/Inseratstart.cpp
@@ -6,7 +6,34 @@ struct Node{
     Node*next;
     Node*prev;
 };
+
+Node*head = nullptr;
+
+void insetatstart(int val){
+    Node*newnode = new Node;
+
+    newnode->data = val;
+    newnode->prev= nullptr;
+    newnode->next = head;
+
+    if (head != nullptr)
+    {
+       head->prev = newnode; //upate the pre 
+    }
+    head = newnode; //this means that we need this line of code in any case
+}
+
+void print(){
+    Node*temp = head;
+
+    while (temp != nullptr)
+    {
+        cout << temp->data;
+        temp = temp->next;
+    }
+}
 int main() {
-    
+    insetatstart(45);
+    print();
     return 0;
 }
